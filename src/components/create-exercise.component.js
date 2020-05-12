025f4f792d878f2415268bc3ca43e9944ee827ac
gaarafive <dattranjd@gmail.com>
2020-05-12 21:33:06 +0700

    add components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>create Exercises</p>
+            </div>
+        );
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ListExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>List Exercise</p>
+            </div>
+        );
+    }
+}
